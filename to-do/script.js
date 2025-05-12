8e36cb32bbe0cf5517d8e57c4b28ed5cc75c0688
Ananditha-phindla <mailtoananditha2004@gmail.com>
2025-05-12 17:13:57 +0530

    updated styling

--- to-do/script.js
+++ to-do/script.js
@@ -5,12 +5,24 @@ const todo = document.querySelector(".todo-list");
 addbutton.addEventListener("click",() => {
     const t = task.value;
     const d = date.value;
+    if(!t && !d){
+        alert("enter your task and date");
+        return;
+    }
+    else if(!t){
+        alert("enter you task");
+        return;
+    }
+    else if(!d){
+        alert("enter the date");
+        return;
+    }
     let p1 = document.createElement("P");
     p1.innerHTML = t;
     let p2 = document.createElement("P");
     p2.innerHTML = d;
     let b = document.createElement("button");
-    b.innerHTML = "DELETE";
+    b.innerHTML = "Delete";
     b.classList.add("deletebutton");
     b.addEventListener("click",() =>{
         p1.remove();
